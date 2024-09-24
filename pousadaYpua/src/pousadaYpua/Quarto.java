d8bf4e17563c4eaf5942483c3f1578f8e5d7cef1
ramos5209 <viniciusaugusto6996@gmail.com>
2024-09-24 19:34:47 -0300

    Create Quarto.java

---
+++ pousadaYpua/src/pousadaYpua/Quarto.java
@@ -0,0 +1,14 @@
+package pousadaYpua;
+
+public class Quarto {
+	private String numero, data;
+	private boolean disponibilidade;
+	
+	public Quarto(String numero, String data,boolean disponibilidade) {
+		this.numero = numero;
+		this.data = data;
+		this.disponibilidade = disponibilidade;
+	}
+	
+
+}
