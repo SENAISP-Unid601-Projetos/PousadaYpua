14d79a8fd4b352ae7fca82671ad62266226aa23d
Pedro Henrique <pedroenriquellopes2011@gmail.com>
2024-09-27 19:24:38 -0300

    banco

--- br.primeiro.database/src/main/java/res/Descrition.sql
+++ br.primeiro.database/src/main/java/res/Descrition.sql
@@ -1,3 +1,4 @@
-create table pearson (id integer, name string);
-insert into pearson values(1, 'leo');
-insert into pearson values(2, 'yui');
+
+create table if not exists pearson (
+id integer,
+name string);
