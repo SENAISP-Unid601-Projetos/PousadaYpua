8980573e3c2677cc54c0889eecb277e8c186ac7b
NicolasRibe <oloconicao@gmail.com>
2024-09-27 19:32:31 -0300

    integra tudo em uma coisa só

---
+++ pousadaYpua/src/pousadaYpua/model/Pearson.java
@@ -0,0 +1,31 @@
+package pousadaYpua.model;
+
+public class Pearson {
+
+    private int id;
+    private String name;
+
+    public Pearson(){
+
+    }
+    public Pearson(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ pousadaYpua/src/pousadaYpua/view/TesteBD.java
@@ -0,0 +1,54 @@
+package pousadaYpua.view;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.Period;
+import java.util.Scanner;
+
+import pousadaYpua.DAO.PearsonDao;
+import pousadaYpua.database.Database;
+import pousadaYpua.model.Pearson;
+
+public class TesteBD {
+;
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		Connection con = null;
+
+		try {
+			con = Database.getinstance().getConnection();
+			Database.prePopulateDatabase();
+			System.out.println("digite o id ?");
+			int id = sc.nextInt();
+			sc.nextLine();
+			System.out.println("digite o nome? ");
+			String name = sc.nextLine();
+
+			Pearson p = new Pearson(id, name);
+
+
+			PearsonDao dao = new PearsonDao();
+			dao.insert(p);
+
+			Statement statement = con.createStatement();
+
+			ResultSet rs = statement.executeQuery("select * from pearson");
+
+			while (rs.next()) {
+				// read the result set
+				System.out.println("name = " + rs.getString("name"));
+				System.out.println("id = " + rs.getInt("id"));
+			}
+		} catch (Exception e) {
+			// if the error message is "out of memory",
+			// it probably means no database file is found
+			e.printStackTrace(System.err);
+		}
+	}
+}
