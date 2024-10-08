0516e453201c2700b124e3bb2f979bc4dc5f7b8e
ramos5209 <viniciusaugusto6996@gmail.com>
2024-10-07 22:24:31 -0300

    Create CadastroCliente.java

---
+++ pousadaYpua/src/pousadaYpua/view/CadastroCliente.java
@@ -0,0 +1,35 @@
+package pousadaYpua.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+
+public class CadastroCliente extends JInternalFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadastroCliente frame = new CadastroCliente();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CadastroCliente() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
