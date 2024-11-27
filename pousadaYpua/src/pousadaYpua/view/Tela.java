83aa8d82deb8d6a23a7cc1a23bf0c751af518046
Vinicius Augusto Ramos Bastos <viniciusaugusto6996@gmail.com>
2024-11-26 22:39:20 -0300

    update

--- pousadaYpua/src/pousadaYpua/view/Tela.java
+++ pousadaYpua/src/pousadaYpua/view/Tela.java
@@ -8,6 +8,7 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.ImageIcon;
 import javax.swing.JDesktopPane;
@@ -86,22 +87,22 @@ public class Tela {
         frame.setContentPane(desktopPane);
         
         lblNewLabel = new JLabel("");
-        lblNewLabel.setIcon(new ImageIcon(Path.getPathResources()+"\\imagemPousada.jpg"));
+        lblNewLabel.setIcon(new ImageIcon(Path.getPathResources()+File.pathSeparator+"imagemPousada.jpg"));
         lblNewLabel.setBounds(0, 0, 744, 355);
         desktopPane.add(lblNewLabel);
         
         lblNewLabel_1 = new JLabel("");
-        lblNewLabel_1.setIcon(new ImageIcon(Path.getPathResources()+"\\pousada2.jpg"));
+        lblNewLabel_1.setIcon(new ImageIcon(Path.getPathResources()+File.pathSeparator+"pousada2.jpg"));
         lblNewLabel_1.setBounds(0, 355, 744, 468);
         desktopPane.add(lblNewLabel_1);
         
         lblNewLabel_2 = new JLabel("");
-        lblNewLabel_2.setIcon(new ImageIcon(Path.getPathResources()+"\\PousadaYpua.jpg"));
+        lblNewLabel_2.setIcon(new ImageIcon(Path.getPathResources()+File.pathSeparator+"PousadaYpua.jpg"));
         lblNewLabel_2.setBounds(745, 0, 795, 355);
         desktopPane.add(lblNewLabel_2);
         
         lblNewLabel_3 = new JLabel("");
-        lblNewLabel_3.setIcon(new ImageIcon(Path.getPathResources()+"\\pousada1.jpg"));
+        lblNewLabel_3.setIcon(new ImageIcon(Path.getPathResources()+File.pathSeparator+"pousada1.jpg"));
         lblNewLabel_3.setBounds(743, 355, 787, 468);
         desktopPane.add(lblNewLabel_3);
         
